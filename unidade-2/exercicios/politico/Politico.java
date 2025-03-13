059daabd7b4a39625f27e8a95e8cf7683d19a749
Levítico Rimon <95304656+LEVEL303@users.noreply.github.com>
2025-03-13 16:16:13 -0300

    Create Politico.java

---
+++ unidade-2/exercicios/politico/Politico.java
@@ -0,0 +1,12 @@
+public class Politico extends Pessoa {
+    private String partido;
+
+    public Politico(String nome, int idade, String partido) {
+        super(nome, idade);
+        this.partido = partido;
+    }
+
+    public String toString() {
+        return super.toString() + "; Partido: " + partido;
+    }
+}
