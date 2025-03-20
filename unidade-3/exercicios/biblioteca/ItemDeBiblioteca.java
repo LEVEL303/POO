73a4ccb5e2e45655d055749ea00fe97c0eff3f91
Levítico Rimon <95304656+LEVEL303@users.noreply.github.com>
2025-03-20 17:13:48 -0300

    Create ItemDeBiblioteca.java

---
+++ unidade-3/exercicios/biblioteca/ItemDeBiblioteca.java
@@ -0,0 +1,13 @@
+public interface ItemDeBiblioteca {
+    int maximoDeDiasParaEmprestimo = 14;
+
+    public boolean estaEmprestado();
+
+    public void empresta();
+
+    public void devolve();
+
+    public String localizacao();
+
+    public String descricao();
+}
